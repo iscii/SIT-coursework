34ec4cadf0a5585ac0550af745b5217c9a3ee2ab
Issac Zheng <48501725+iscii@users.noreply.github.com>
2021-12-15 20:39:56 -0500

    Delete CS 115/assessments directory

--- CS 115/assessments/quiz2.py
+++
@@ -1,9 +0,0 @@
-
-def test(x, n):
-    if(n==0):
-        result=1
-    else:
-        result=x*test(x, n-1)
-    return result
-
-print(test(2, 0))
--- CS 115/assessments/quiz6.py
+++
@@ -1,8 +0,0 @@
-L = (2,5,3,1,4) #i=4
-L2 = (6,8,0,7,9) #i=4
-x=4
-x2=9
-for i in range(len(L)):
-    if L[i]==x:
-        break
-    
