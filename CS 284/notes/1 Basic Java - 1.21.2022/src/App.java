920d2fece253b7f4ded6924fa91e5e3735a482ee
Issac Zheng <izheng@castlepointanime.com>
2022-01-22 11:28:19 -0500

    284 update

---
+++ CS 284/notes/1 Basic Java - 1.21.2022/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Person a = new Person("hi");
+        //Person b = new Person(); //- > throws error
+    }
+}
