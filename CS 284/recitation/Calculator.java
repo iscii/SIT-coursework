8cd98fc5d39d28aa63ad6c8cf8687cdbc882f640
iscii <issac.zheng9869@gmail.com>
2022-01-28 14:44:35 -0500

    Create Calculator.java

---
+++ CS 284/recitation/Calculator.java
@@ -0,0 +1,25 @@
+import java.io.Console;
+
+public class Calculator{
+    int base;
+
+    public Calculator(int base){
+        this.base = base;
+    }
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static float divide(int a, int b){
+        if(b == 0){
+            return -1;
+        }
+        return a/b;
+    }
+
+    public static void main(String[] args){
+        Calculator a = new Calculator(2);
+        System.out.println(Calculator.add(1,2));
+    }
+}
